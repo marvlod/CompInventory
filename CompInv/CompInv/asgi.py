7b5e890725f52f75b074b3991da47390d908dbb2
Владимир Мартынов <voldyr@live.ru>
2023-02-15 14:51:03 +0300

    Переименование проекта

--- CompInv/CompInv/asgi.py
+++ CompInv/CompInv/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ComInv project.
+ASGI config for CompInv project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ComInv.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CompInv.settings')
 
 application = get_asgi_application()
--- CompInv/CompInv/wsgi.py
+++ CompInv/CompInv/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ComInv project.
+WSGI config for CompInv project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ComInv.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CompInv.settings')
 
 application = get_wsgi_application()
